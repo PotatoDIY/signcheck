c248f67c962e5e9e666e508e1afc03bd4015919d
土豆电工 <tudoudiangong@163.com>
2019-12-10 07:27:27 +0800

    ..

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SignCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSignString = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Location = new System.Drawing.Point(27, 50);
+            this.textBoxString.Multiline = true;
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(750, 125);
+            this.textBoxString.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SignString";
+            // 
+            // textBoxSignString
+            // 
+            this.textBoxSignString.Location = new System.Drawing.Point(27, 226);
+            this.textBoxSignString.Multiline = true;
+            this.textBoxSignString.Name = "textBoxSignString";
+            this.textBoxSignString.Size = new System.Drawing.Size(750, 125);
+            this.textBoxSignString.TabIndex = 2;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(27, 376);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(750, 44);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // labelOut
+            // 
+            this.labelOut.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelOut.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelOut.Location = new System.Drawing.Point(27, 442);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(750, 70);
+            this.labelOut.TabIndex = 5;
+            this.labelOut.Text = "false";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSignString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxString);
+            this.Name = "Form1";
+            this.Text = "SignCheck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSignString;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Label labelOut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            if (signCheck(textBoxString.Text,textBoxSignString.Text))
+            {
+                labelOut.Text = "True";
+            }
+            else
+            {
+                labelOut.Text = "False";
+            }
+        }
+
+        public bool signCheck(string str, string sign)
+        {
+            try
+            {
+                byte[] bt = Encoding.GetEncoding("utf-8").GetBytes(str);
+                var sha256 = new SHA256CryptoServiceProvider();
+                byte[] rgbHash = sha256.ComputeHash(bt);
+
+                RSACryptoServiceProvider key = new RSACryptoServiceProvider();
+
+                using (StreamReader streamReader = new StreamReader("PublicKey.xml")) // 读取运行目录下的PrivateKey.xml
+                {
+                    key.FromXmlString(streamReader.ReadToEnd());// 将公匙载入进RSA实例中
+                }
+                RSAPKCS1SignatureDeformatter deformatter = new RSAPKCS1SignatureDeformatter(key);
+                deformatter.SetHashAlgorithm("SHA256");
+                byte[] rgbSignature = Convert.FromBase64String(sign);
+                if (deformatter.VerifySignature(rgbHash, rgbSignature))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
